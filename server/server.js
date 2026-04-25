d6d258faab6fe1055b2a291e1f19d584ab620d99
just-a-guy150 <dimonmig301@gmail.com>
2026-04-25 13:48:31 +0300

    v1.0001

--- server/server.js
+++ server/server.js
@@ -10,6 +10,14 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('static'))
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); 
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); 
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
+    next();
+})
+
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
@@ -70,66 +78,86 @@ app.get(
     getMovie
 )
 
-app.get('/movies', (req, res) => {
-    let query = "SELECT * FROM movies"
-    let queryParams = []
-        
-    if (req.query.sort) {
-        if (
-            ["id", "title","rating"].includes(req.query.sort) 
-        ) {
-            query += ` ORDER BY ${req.query.sort}`
-        } else {
-            return res.status(400).send("Invalid sort field")
-        }
+app.get("/movies", (req, res) => {
+	let query = "SELECT * FROM movies"
+	let queryParams = []
+
+	if (req.query.sort) {
+		if (
+			["id", "title", "raiting"].includes(req.query.sort)
+		) {
+			query += " ORDER BY " + req.query.sort
+		} else {
+			return res.status(400).send("Invalid sort field")
+		}
+	}
+
+	if (req.query.sortType) {
+		const sortType = req.query.sortType.toUpperCase()
+
+		if (
+			["ASC", "DESC"].includes(sortType)
+		) {
+			query += " " + sortType
+		} else {
+			return res.status(400).send("Invalid sort type")
+		}
+	}
+
+	if (req.query.limit) {
+		const limit = parseInt(req.query.limit, 10)
+
+		if (
+			!isNaN(limit) && limit > 0
+		) {
+			query += " LIMIT ?"
+			queryParams.push(limit)
+		} else {
+			return res.status(400).send("Invalid limit")
+		}
+	}
+
+	if (req.query.offset) {
+		const offset = parseInt(req.query.offset, 10)
+
+		if (
+			!isNaN(offset) && offset > 0
+		) {
+			query += " OFFSET ?"
+			queryParams.push(offset)
+		} else {
+			return res.status(400).send("Invalid limit")
+		}
+	}
+
+	db.query(query, queryParams, (err, results) => {
+		if(err) {
+			console.error("SQL Error", err)
+			res.status(500).send("Intenal Server Error")
+		} else {
+			res.json(results)
+		}
+	})
+
+})
+
+app.get("/search", (req, res) => {
+    if (!req.query.title) {
+        return res.status(400).send("Title parameter is required")
     }
 
-    if (req.query.sortType) {
-        const sortType = req.query.sortType.toLowerCase()
-        
-        if (
-            ["ASC", "DESC"].includes(sortType)
-        ) {
-            query += " " + sortType
-        } else {
-            return res.status(400).send("Invalid sort type")
-        }
-    }
+    const title = `%${req.query.title}%`
+    const query = "SELECT * FROM movies WHERE title LIKE ? LIMIT 10"
 
-    if (req.query.limit) {
-        const limit = parseInt(req.query.limit, 10)
-        
-        if (
-            !isNaN(limit) && limit > 0
-        ) {
-            query += " LIMIT ?"
-            queryParams.push(limit)
-        } else {
-            return res.status(400).send("Invalid limit")
-        }
-    }
-
-    if (req.query.offset) {
-        const offset = parseInt(req.query.offset, 10)
-        
-        if (
-            !isNaN(offset) && offset > 0
-        ) {
-            query += " OFFSET ?"
-            queryParams.push(offset)
-        } else {
-            return res.status(400).send("Invalid limit")
-        }
-    }
-
-    db.query(query, queryParams, (err, result) => {
+    db.query(query, [title], (err, result) => {
         if (err) {
-            console.log("Sql error", err)
-            res.status(500).send("Internal Server Error")
+            console.log(err)
+            return res.status(500).send("Internal Server Error")
         } else {
+
             res.json(result)
         }
-    })
+    })  
 })
 
 app.listen(port, () => {
